2221ac9d70e0d34870a296b9f7278d1c6d5f8a76
JuanDYB <JuanDYB@users.noreply.github.com>
2019-04-04 11:52:33 +0200

    Update SpeedwayReaderManager.cs
    Better connection manage
    Tag report events in blocking collection

--- SpeedwayReaderManager.cs
+++ SpeedwayReaderManager.cs
@@ -1,9 +1,8 @@
 using Impinj.OctaneSdk;
 using NLog;
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace WS.Reader.Procedure
@@ -12,19 +11,29 @@ namespace WS.Reader.Procedure
     {
         private static NLog.Logger logger = LogManager.GetCurrentClassLogger();
 
+        private const string READER_USERNAME = "root";
+        private const string READER_PASSWORD = "impinj";
+
         //Properties
         private Reader ReaderInformation;
+        private readonly ushort ReaderConnectTimeoutSecs;
         private readonly uint KeepAliveSecs;
         private readonly ushort MaxKeepAliveLost;
+        private readonly ushort RebootAfterReconnects;
 
         //ImpinjAPI
-        private ImpinjReader _impinj;
+        private ImpinjReader _impinjReader;
 
         //Estado
         internal bool Running { get; set; }
         internal bool IsConnected { get; set; }
-        internal bool InventoryRunning;
         private System.Timers.Timer _checkNetworkTimer;
+        private int ReconnectRetries = 0;
+
+        //Inventario
+        internal bool InventoryRunning { get; private set; }
+        private IList<string> ReadedTags { get; set; }
+        private static BlockingCollection<string> _tagInventoryThreadSafe;
 
         //GPIO
         private Dictionary<UInt16, Boolean> GPIOPortStatus = new Dictionary<UInt16, Boolean>();
@@ -40,63 +49,189 @@ namespace WS.Reader.Procedure
         //Event Handlers
         internal event EventHandler<ReaderEventArgs> GPIEvent;
         internal event EventHandler<ReaderEventArgs> TagReportEvent;
+        internal event EventHandler NewTagReadedEvent;
+        internal event EventHandler<ReaderEventArgs> InventoryFinishEvent;
 
-        public SpeedwayReaderManager(Reader readerInfo, uint KeepAliveSecs, ushort MaxKeepAliveLost)
+        public SpeedwayReaderManager(Reader readerInfo, ushort ReaderConnectTimeoutSecs, uint KeepAliveSecs, ushort MaxKeepAliveLost, ushort RebootAfterReconnects)
         {
             this.ReaderInformation = readerInfo;
+            this.ReaderConnectTimeoutSecs = ReaderConnectTimeoutSecs;
             this.KeepAliveSecs = KeepAliveSecs;
             this.MaxKeepAliveLost = MaxKeepAliveLost;
+            this.RebootAfterReconnects = RebootAfterReconnects;
             this.IsConnected = false;
-            this._impinj = new ImpinjReader(this.ReaderInformation.IP, this.ReaderInformation.NAME);
+            this._impinjReader = new ImpinjReader(this.ReaderInformation.IP, this.ReaderInformation.NAME);
         }
 
         #region Reader Connect
-        internal void Connect()
+
+        internal void StartReader()
         {
-            if (!this.IsConnected)
+            try
+            {
+                this.Running = true;
+                this.ReaderConnect();
+                this.InitReader();
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex, "[START READER ERROR]");
+            }
+        }
+
+        /// <summary>
+        /// Disconnect Reader and Quit events. END READER
+        /// </summary>
+        internal void EndReader()
+        {
+            try
+            {
+                this.Running = false;
+                this.UnsetReaderEvents();
+                this.ReaderDisconnect();
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex, "[END READER ERROR]");
+            }
+        }
+
+        /// <summary>
+        /// Connect to the reader
+        /// </summary>
+        private void ReaderConnect()
+        {
+            if (!this.IsConnected && this._impinjReader != null)
             {
                 try
                 {
-                    logger.Info("[CONECTANDO CON READER]");
-                    this._impinj.ConnectTimeout = 2000;
-                    this._impinj.Connect();
-                    this.InitializeReaderParameters();
-                    this.SetReaderEvents();
+                    logger.Info("[CONECTANDO CON READER]: {0}-{1}", this._impinjReader.Name, this._impinjReader.Address);
+                    this._impinjReader.ConnectTimeout = this.ReaderConnectTimeoutSecs * 1000;
+                    this._impinjReader.Connect();
                     this.IsConnected = true;
+                    this.ReconnectRetries = 0;
+                    logger.Info("[READER CONECTADO!!]: {0}-{1}", this._impinjReader.Name, this._impinjReader.Address);
+                    this.CheckReaderStatus();
+                    
                 }
                 catch (Exception ex)
                 {
-                    logger.Error(ex, "[CONEXION FALLIDA CON READER]: {0}", this.ReaderInformation.IP);
+                    logger.Error(ex, "[CONEXION FALLIDA CON READER]: {0}-{1}", this._impinjReader.Name, this._impinjReader.Address);
                     this.IsConnected = false;
+                    this.ReconnectRetries++;
                     this.StartCheckNetwork();
+                    throw;
                 }
             }
-            logger.Trace("FIN del Metodo Reader Connect");
         }
 
-        internal void Disconnect()
+        /// <summary>
+        /// Check reader status. Call this method after connect
+        /// </summary>
+        private void CheckReaderStatus()
         {
-            this.Running = false;
-            logger.Info("[DESCONEXION]: {0}", this.ReaderInformation.IP);
-            if (this.InventoryRunning)
+            Impinj.OctaneSdk.Status _readerStatus = this._impinjReader.QueryStatus();
+            //Check if reader is in inventory and stops it
+            if (!this.InventoryRunning && _readerStatus.IsSingulating)
             {
+                logger.Warn("[INVENTORY STOPED WHILE READER WAS OFFLINE] Trying to stop");
                 this.StopInventory();
             }
-            this.UnsetReaderEvents();
-            _impinj.Disconnect();
+
+            //Resume reader events
+            this._impinjReader.ResumeEventsAndReports();
+
+            //Log Reader temperature
+            logger.Info("READER TEMPERATURE: {0}ºC", _readerStatus.TemperatureInCelsius);
+
+            //Log Antenna information
+            foreach(Impinj.OctaneSdk.AntennaStatus _antStatus in _readerStatus.Antennas)
+            {
+                logger.Info("ANTENNA STATUS [PORT: {0} | CONNECTED: {1}]", _antStatus.PortNumber, _antStatus.IsConnected);
+            }
+
+            //Log GPI information
+            foreach(Impinj.OctaneSdk.GpiStatus _gpiStatus in _readerStatus.Gpis)
+            {
+                logger.Info("GPI STATUS [PORT: {0} | STATUS: {1}]", _gpiStatus.PortNumber, _gpiStatus.State);
+            }
         }
+
+        /// <summary>
+        /// Initialize Reader. Assign Events and configure parameters
+        /// </summary>
+        private void InitReader()
+        {
+            if (this.IsConnected && this._impinjReader != null)
+            {
+                try
+                {
+                    this.InitializeReaderParameters();
+                    this.SetReaderEvents();
+                }
+                catch (Exception ex)
+                {
+                    logger.Error(ex, "[ERROR CONFIGURE READER]");
+                    this.UnsetReaderEvents();
+                    throw;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Reader Disconnect
+        /// </summary>
+        private void ReaderDisconnect()
+        {
+            logger.Info("[DESCONEXION]: {0}-{1}", this._impinjReader.Name, this._impinjReader.Address);
+            this.IsConnected = false;
+            try
+            {
+                this.StopCheckNetworkTimer();
+                _impinjReader.Disconnect();
+            }
+            catch(Exception ex)
+            {
+                logger.Error(ex, "[ERROR DISCONNECT READER]");
+                throw;
+            }
+        }
+
+        private void ReconnectAfterConnectionLost()
+        {
+            try
+            {
+                if (!this.IsConnected && this.Running)
+                {
+                    if (this.Running)
+                    {
+                        this.ReaderConnect();
+                    }
+                    else
+                    {
+                        this.StartReader();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex, "[ERROR RECONNECT READER]");
+            }
+        }
+
         #endregion
 
 
         #region Reader Configuration
         private void InitializeReaderParameters()
         {
-            _impinj.DeleteAllOpSequences();
+            logger.Info("[CONFIGURANDO READER]");
+            _impinjReader.DeleteAllOpSequences();
 
             // Get the default settings
             // We'll use these as a starting point
             // and then modify the settings we're interested in.
-            Settings settings = _impinj.QueryDefaultSettings();
+            Settings settings = _impinjReader.QueryDefaultSettings();
             var parameters = ReaderInformation.T_READER_PARAMETERS[0];
 
             // Set the start trigger to Immediate.
@@ -138,24 +273,33 @@ namespace WS.Reader.Procedure
             _keepaliveCfg.LinkDownThreshold = this.MaxKeepAliveLost;
             settings.Keepalives = _keepaliveCfg;
 
+            // If this application disconnects from the 
+            // reader, hold all tag reports and events.
+            settings.HoldReportsOnDisconnect = true;
+
+            //No Autostop
+            settings.AutoStop.Mode = AutoStopMode.None;
+
             // Apply the newly modified settings.
-            _impinj.ApplySettings(settings);
+            _impinjReader.ApplySettings(settings);
         }
 
         private void SetReaderEvents()
         {
+            logger.Info("[ASIGNACION EVENTOS READER]");
             //Eventos de conexion
-            this._impinj.ConnectionLost += new ImpinjReader.ConnectionLostHandler(Connection_lost);
-            //this._impinj.KeepaliveReceived += new ImpinjReader.KeepaliveHandler(Connection_lost);
+            this._impinjReader.ConnectionLost += _impinjReader_ConnectionLost;
+            this._impinjReader.KeepaliveReceived += _impinjReader_KeepaliveReceived;
 
             //Eventos de GPI
-            this._impinj.GpiChanged += OnGPIChanged;
+            this._impinjReader.GpiChanged += OnGPIChanged;
         }
 
         private void UnsetReaderEvents()
         {
-            this._impinj.ConnectionLost -= Connection_lost;
-            this._impinj.GpiChanged -= OnGPIChanged;
+            this._impinjReader.ConnectionLost -= _impinjReader_ConnectionLost;
+            this._impinjReader.KeepaliveReceived -= _impinjReader_KeepaliveReceived;
+            this._impinjReader.GpiChanged -= OnGPIChanged;
         }
 
         #endregion
@@ -167,13 +311,22 @@ namespace WS.Reader.Procedure
             logger.Info("[INICIO INVENTARIO]");
             try
             {
-                this._impinj.TagsReported += OnTagsReported;
-                this._impinj.Start();
+                _tagInventoryThreadSafe = new BlockingCollection<string>();
+                this.ReadedTags = new List<string>();
+                this._impinjReader.TagsReported += OnTagsReported;
+                this._impinjReader.Start();
+                Task.Run(() => ProcessTagsTask(this));
                 this.InventoryRunning = true;
             }
             catch (Exception ex)
             {
                 logger.Fatal(ex, "ERROR INICIANDO INVENTARIO");
+                this.InventoryRunning = false;
+                this._impinjReader.TagsReported -= OnTagsReported;
+                this.ReadedTags = null;
+                _tagInventoryThreadSafe.CompleteAdding();
+                _tagInventoryThreadSafe.Dispose();
+                _tagInventoryThreadSafe = null;
                 throw;
             }
         }
@@ -184,29 +337,67 @@ namespace WS.Reader.Procedure
             
             try
             {
-                this._impinj.TagsReported -= OnTagsReported;
-                this._impinj.Stop();
                 this.InventoryRunning = false;
+                if (_tagInventoryThreadSafe != null)
+                {
+                    _tagInventoryThreadSafe.CompleteAdding();
+                }
+                this._impinjReader.TagsReported -= OnTagsReported;
+                this._impinjReader.Stop();
             }
             catch (Exception ex)
             {
                 logger.Fatal(ex, "ERROR PARANDO INVENTARIO");
+                this.InventoryRunning = false;
                 throw;
             }
+        }
+
+        private static void ProcessTagsTask(SpeedwayReaderManager readerManager)
+        {
+            if(_tagInventoryThreadSafe != null)
+            {
+                foreach(string _tagEPC in _tagInventoryThreadSafe.GetConsumingEnumerable())
+                {
+                    if (!readerManager.ReadedTags.Contains(_tagEPC))
+                    {
+                        logger.Info("[UNIQUE TAG FOUND]: {0}", _tagEPC);
+                        readerManager.ReadedTags.Add(_tagEPC);
+                        readerManager.NewTagReadedEvent?.Invoke(readerManager, new EventArgs());
+                    }
+                }
+                _tagInventoryThreadSafe.Dispose();
+                _tagInventoryThreadSafe = null;
+            }
+            logger.Info("ProcessTagsTask FINISHED!!");
+            readerManager.InventoryFinishEvent?.Invoke(readerManager, new ReaderEventArgs { ReaderInventoryEPC = readerManager.ReadedTags });
         }
 
         #endregion
 
         #region Reader Events
-        private void Connection_lost(ImpinjReader reader)
+        private void _impinjReader_ConnectionLost(ImpinjReader reader)
         {
-            logger.Error("[CONEXION PERDIDA]: {0}", this.ReaderInformation.IP);
-            this.IsConnected = false;
+            logger.Error("[CONEXION PERDIDA]: {0}-{1}", reader.Name, reader.Address);
+            try
+            {
+                this.ReaderDisconnect();
+            }
+            catch (Exception ex)
+            {
+                logger.Fatal(ex, "CONNECTION LOST. DISCONNECT ERROR");
+            }
             this.StartCheckNetwork();
+        }
+
+        private void _impinjReader_KeepaliveReceived(ImpinjReader reader)
+        {
+            //logger.Trace("[KeepAliveReceived]: {0}-{1}", reader.Name, reader.Address);
         }
 
         private void OnGPIChanged(ImpinjReader reader, GpiEvent e)
         {
+            this.SavePortSatus(e.PortNumber, e.State);
             ReaderEventArgs _evtArgs = new ReaderEventArgs { GPIPortNumber = e.PortNumber, GPIPortStatus = e.State };
             GPIEvent?.Invoke(this, _evtArgs);
         }
@@ -215,24 +406,35 @@ namespace WS.Reader.Procedure
         {
             if (this.InventoryRunning)
             {
-                List<string> ReadedTags = new List<string>();
                 foreach (Tag tag in report)
                 {
                     string HexEpc = tag.Epc.ToHexString();
-                    if (!ReadedTags.Any(X => X == HexEpc))
+                    //logger.Trace("[TAG DETECTED]: {0} {1}", HexEpc, this.InventoryRunning);
+                    try
                     {
-                        ReadedTags.Add(HexEpc);
-                        //this.LogMessage("Info", String.Format("[TAG DETECTED]: {0}", HexEpc));
+                        if (!_tagInventoryThreadSafe.IsAddingCompleted)
+                        {
+                            _tagInventoryThreadSafe.Add(HexEpc);
+                        }
+                        else
+                        {
+                            logger.Warn("ADD TO BLOCK COLLECTION NOT POSSIBLE [{0}]", HexEpc);
+                        }
                     }
-                }
-                if (ReadedTags.Any())
-                {
-                    GPIEvent?.Invoke(this, new ReaderEventArgs { ReaderInventoryEPC = ReadedTags });
+                    catch (Exception ex)
+                    {
+                        logger.Error(ex, "ERROR ADDING TAG TO BLOCKCOLLECTION [{0}]", HexEpc);
+                    }
                 }
             }
         }
         #endregion
 
+        /// <summary>
+        /// Save GPI Status on Dictionary on every Change
+        /// </summary>
+        /// <param name="PortNumber"></param>
+        /// <param name="state"></param>
         private void SavePortSatus(ushort PortNumber, bool state)
         {
             if (this.GPIOPortStatus == null)
@@ -259,15 +461,60 @@ namespace WS.Reader.Procedure
             bool ChkNetwork = NetworkUtils.DoPing(this.ReaderInformation.IP);
             if (ChkNetwork)
             {
-                this._checkNetworkTimer.AutoReset = false;
-                this._checkNetworkTimer.Enabled = false;
-                this._checkNetworkTimer.Dispose();
-                if (!IsConnected)
+                this.StopCheckNetworkTimer();
+
+                //Reboot if reconnects exceed
+                if(this.RebootAfterReconnects > 0 && this.ReconnectRetries > this.RebootAfterReconnects)
                 {
-                    this.Connect();
+                    this.ReconnectRetries = 0;
+                    this.SendRShellCommand("reboot");
                 }
+                this.ReconnectAfterConnectionLost();
             }
         }
+
+        private void StopCheckNetworkTimer()
+        {
+            this._checkNetworkTimer.AutoReset = false;
+            this._checkNetworkTimer.Enabled = false;
+            this._checkNetworkTimer.Dispose();
+        }
+        #endregion
+
+        #region RSHELL
+
+        /// <summary>
+        /// Send command to reader via Rshell
+        /// </summary>
+        /// <param name="command"></param>
+        private void SendRShellCommand(string command)
+        {
+            try
+            {
+                logger.Info("SENDING RSHELL COMMAND [{0}]...", command);
+                string reply = String.Empty; 
+                this._impinjReader.RShell.OpenSecureSession(this.ReaderInformation.IP, READER_USERNAME, READER_PASSWORD, 5000);
+                RShellCmdStatus status = this._impinjReader.RShell.Send(command, out reply);
+                if (status == RShellCmdStatus.Success)
+                {
+                    logger.Info("RSHELL COMMAND SUCESSFUL [{0}]...", command);
+                }
+                else
+                {
+                    logger.Error("RSHELL COMMAND FAILED [{0}]...", command);
+                }
+                logger.Info("RSHELL COMMAND REPLY [{0}]...", reply);
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex, "ERROR SENDING RSHELL COMMAND [{0}]...", command);
+            }
+            finally
+            {
+                this._impinjReader.RShell.Close();
+            }
+        }
+
         #endregion
     }
 }
